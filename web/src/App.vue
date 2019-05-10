2832d46f271af6213f2bf1f611de4ce51d255626
dennis <crabby94@outlook.de>
2019-05-10 14:58:11 +0200

    #2 - add router

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,6 +17,7 @@
         </v-img>
   <v-container>
 </template>
+
 <script>
 export default {
   name: 'App',
--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import VueRouter from 'vue-router'
 
 Vue.use(Vuetify)
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
