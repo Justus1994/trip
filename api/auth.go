d2345287a3513f6b28d45175720066d8e459f820
Niko Ruthardt <buybypenny@googlemail.com>
2019-05-12 17:44:44 +0200

    update autorization
    added db for data model and update auth

--- api/auth.go
+++ api/auth.go
@@ -1,24 +1,48 @@
 package main
 
-import(
-  "log"
-  "net/http"
-  "os/exec"
+import (
+	"log"
+	"net/http"
+	"os/exec"
 )
 
-// Auth : return value of Authorization header or a new User
+var dbToken = "validtoken"
+
+// Auth : return : value of Authorization header or a new token
 func Auth(r *http.Request) []byte {
-  token := []byte(r.Header.Get("Authorization"))
-  if len(token) > 0 {
-    return token
-  }
-  return user()
+	token := []byte(r.Header.Get("Authorization"))
+
+	if AuthToken(token) {
+		return token
+	}
+
+	newToken := newToken()
+	return save(newToken)
 }
 
-func user() []byte{
-  out, err := exec.Command("uuidgen").Output()
-     if err != nil {
-         log.Fatal(err)
-     }
-     return out
+//AuthToken : authoring a token return : true if authorized
+func AuthToken(token []byte) bool {
+	exist := redisClient.SIsMember(dbToken, string(token)).Val()
+
+	if exist {
+		return true
+	}
+
+	return false
+}
+
+func newToken() []byte {
+	out, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		log.Fatal("Can't create new Token", err)
+	}
+	//remove last char from string because exec.command returns with newLine
+	return out[:len(out)-1]
+}
+
+func save(token []byte) []byte {
+	log.P
+	redisClient.SAdd(dbToken, token)
+	redisClient.Set(string(token), 0, 0)
+	return token
 }
--- api/main.go
+++ api/main.go
@@ -1,37 +1,39 @@
 package main
 
-import(
-  "log"
-  "net/http"
+import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+var redisClient = newClient()
+
 func newRouter() *mux.Router {
 
-  /***** All Endpoints are available under localhost:8080/api if deployed via docker compose *****/
-  r := mux.NewRouter()
+	/***** All Endpoints are available under localhost:8080/api if deployed via docker compose *****/
+	r := mux.NewRouter()
 
-  /***** Handle Trip ******/
-//  r.HandleFunc("/trip/{tag}", newTrip).Methods("CREATE")
-//  r.HandleFunc("/trip/{id}", getTrip).Methods("GET")
+	/***** Handle Trip ******/
+	//r.HandleFunc("/trip/{tag}", TripHandler).Methods("POST")
+	//r.HandleFunc("/trip/{id}", getTrip).Methods("GET")
 
-  /***** Handle Nodes ******/
-//	r.HandleFunc("/trip/{id}/extend/", createNodeHandler).Methods("GET")
-//	r.HandleFunc("/trip/{id}/nodes/{id}", deleteNodeHandler).Methods("DELETE")
+	/***** Handle Nodes ******/
+	//	r.HandleFunc("/trip/{id}/extend/", createNodeHandler).Methods("GET")
+	//	r.HandleFunc("/trip/{id}/nodes/{id}", deleteNodeHandler).Methods("DELETE")
 
-  /***** Handle User *****/
-  r.HandleFunc("/auth", auth).Methods("GET")
+	/***** Handle authentication *****/
+	r.HandleFunc("/auth", auth).Methods("GET")
 
 	return r
 }
 
 func auth(w http.ResponseWriter, r *http.Request) {
-    w.Write(Auth(r))
+	w.Write(Auth(r))
 }
 
 func main() {
 	router := newRouter()
-
 	log.Println("server listening on port 8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
 
