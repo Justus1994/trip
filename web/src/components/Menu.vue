d1902e8412c03a3a02b567d4f3692f324becff6a
Niko Ruthardt <buybypenny@googlemail.com>
2019-06-24 18:46:39 +0200

    move menu to components

---
+++ web/src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <!--      Menu:    -->
+  <div v-darkmode="darkmode" class="menu">
+    <v-btn v-darkmode="darkmode" flat @click="$emit('toggleDarkmode')">{{darkmode? 'lightmode': 'darkmode'}}</v-btn>
+    <v-btn v-darkmode="darkmode" flat @click="$emit('closeMenu')">Close</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Menu",
+    props: ['darkmode'],
+
+}
+</script>
+
+<style lang="css" scoped>
+  .menu{
+    transition: all 0.5s cubic-bezier(0.25, 0.1, 0, 1.2);
+    position: absolute;
+    height: 100vh;
+    top: 0;
+    left: -33%;
+    width: 33%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
